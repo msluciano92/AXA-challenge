f73757fe4e3c7dc6fcf7897e6774ca345c7502a6
msluciano92 <m_s_luciano92@hotmail.com>
2022-01-14 09:10:52 +0100

    Feat (movies): Integrating sell functionality

--- movies-app/src/App.jsx
+++ movies-app/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
 import React, {useState, useEffect} from 'react'
-import {Card, Grid, Typography} from '@material-ui/core'
+import {Card, Grid, Typography, Button, Box, Modal, MenuItem, Select} from '@material-ui/core'
 import StyledAvatar from './components/StyledAvatar';
 import CinemaService from './services/cinema'
+import FormControl from '@material-ui/core/FormControl';
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [studios, setStudios] = useState([]);
   const [avatarSize, setAvatarSize] = useState(280);
   const [cardStyle, setCardStyle] = useState('regularCard');
+  const [selectedStudio, setSelectedStudio] = useState(1);
+  const [open, setOpen] = React.useState(false);
 
   const responsiveStyle = () => {
     //TODO: produce a better resize strategy
@@ -20,6 +23,10 @@ function App() {
       setAvatarSize(280)
       setCardStyle('regularCard')
     }
+  }
+
+  const transferMovie = (data) => {
+    console.log(data);
   }
 
   useEffect(() => {
@@ -36,7 +43,7 @@ function App() {
       <div className="App-studios App-flex"> 
         <h3>Images:</h3>
         <Grid container justify="center" alignItems="center">
-          {movies.map(movie =>
+          {movies.map((movie, i) =>
             <Grid item xs={12} sm={6} lg={4}>
               <Card className={cardStyle}>
                 <StyledAvatar
@@ -60,6 +67,38 @@ function App() {
                     return studio.name
                   }
                 })}</Typography>
+                <Button onClick={setOpen} variant="contained">Sell film for $ {movie.price}</Button>
+                <Modal
+                  aria-labelledby="unstyled-modal-title"
+                  aria-describedby="unstyled-modal-description"
+                  open={open}
+                  onClose={() => {setOpen(!open)}}
+                >
+                  <Box className="modal">
+                    <h2 id="unstyled-modal-title">Are you sure you want to sell this movie for $ {movie.price}</h2>
+                    <div>
+                      <FormControl>
+                        Select a company
+                        <Select
+                          labelId="demo-customized-select-label"
+                          id="demo-customized-select"
+                          className="margin-top"
+                          value={selectedStudio}
+                          onChange={(e) => {setSelectedStudio(e.target.value)}}
+                          // input={<BootstrapInput />}
+                        >
+                          <MenuItem value={1}>Sony</MenuItem>
+                          <MenuItem value={2}>Disney</MenuItem>
+                          <MenuItem value={3}>Warner bros</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </div>
+                    <div className="margin-top">
+                      <Button onClick={() => {transferMovie({moviePosition: i, targetStudio: selectedStudio})}} className="margin-left" variant="contained">Sell</Button>
+                      <Button className="margin-left" onClick={() => {setOpen(!open)}} variant="contained">Cancel</Button>
+                    </div>
+                  </Box>
+                </Modal>
               </Card>
             </Grid>)}
         </Grid>
