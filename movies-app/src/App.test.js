d72161dca30a5eb215510f0c1db212bbd517949f
msluciano92 <m_s_luciano92@hotmail.com>
2022-01-15 09:13:31 +0100

    Feat (test): Added app test

---
+++ movies-app/src/App.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+describe("App Component", function () {
+  it("should have Images: tag", function () {
+    let { getByText } = render(<App />);
+    expect(getByText("Images:")).toBeDefined();
+  });
+});
